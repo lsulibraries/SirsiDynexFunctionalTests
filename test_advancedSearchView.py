a308463b9993d21d6593874100d94b0ad24ce118
GarrettArm <garrett.w.armstrong@gmail.com>
2018-07-18 11:11:15 -0500

    begin tests on new pages

---
+++ test_advancedSearchView.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+
+from selenium import webdriver
+import pytest
+
+
+@pytest.fixture
+def load_driver(request):
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("browser.cache.disk.enable", False)
+    profile.set_preference("browser.cache.memory.enable", False)
+    profile.set_preference("browser.http.user-cache", False)
+    driver = webdriver.Firefox()
+    # driver.delete_all_cookies()
+    driver.get('https://lsu.ent.sirsi.net/client/en_US/lsu/?rm=ADVANCED+SEARCH0|||1|||0|||true')
+
+    def fin():
+        print('teardown driver')
+        driver.close()
+
+    request.addfinalizer(fin)
+    return driver
+
+
+def test_page_loads(load_driver):
+    driver = load_driver
+    assert 'Advanced Search' in driver.title
+
+
+def test_hideBasicSearch(load_driver):
+    driver = load_driver
+    searchBoxWrapper_divs = driver.find_elements_by_class_name('hideBasicSearch')
+    for div in searchBoxWrapper_divs:
+        assert div.getCssValue('display') == 'none'
