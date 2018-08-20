ad14abc3454e28a0309abdae76f0b539481635a2
GarrettArm <garrett.armstrong@gmail.com>
2018-08-20 15:00:36 -0500

    some new tests & fixing error in custom

--- test_results_page.py
+++ test_results_page.py
@@ -1,11 +1,6 @@
 #! /usr/bin/env python3
 
 from selenium import webdriver
-# from selenium.webdriver.support.ui import Select
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support import expected_conditions as EC
 import pytest
 
 
@@ -43,6 +38,7 @@ def load_access_page(request):
     request.addfinalizer(fin)
     return driver
 
+################################################################################
 
 def test_page_loads(load_driver):
     driver = load_driver
