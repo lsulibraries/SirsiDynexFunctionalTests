1295aae9c46327e833d866be936ba081835ccecb
GarrettArm <garrett.w.armstrong@gmail.com>
2018-10-12 15:39:40 -0500

    uncommented a line

--- our_files/custom.js
+++ our_files/custom.js
@@ -29,7 +29,7 @@ var doDetailViewTasks = function() {
   linkAvailableOnlineCallNumber();
   replaceAvailableStatus();
   renameItemHoldsColumn();
-  // scheduleStackMapToCurrentLocation = setInterval(moveStackMapToCurrentLocation, 200);
+  scheduleStackMapToCurrentLocation = setInterval(moveStackMapToCurrentLocation, 200);
 
 }
 
