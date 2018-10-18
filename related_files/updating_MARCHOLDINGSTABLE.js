011daa30022ca8999d8c362888c2cfef9dfbd8b8
GarrettArm <garrett.w.armstrong@gmail.com>
2018-10-18 12:54:14 -0500

    emphasizing 863 item in callno column

--- related_files/updating_MARCHOLDINGSTABLE.js
+++ related_files/updating_MARCHOLDINGSTABLE.js
@@ -15,18 +15,38 @@ var textualHoldings = '';
 var textualHoldingsEnumeration = '';
 var textualHoldingsSupplemental = '';
 var textualHoldingsIndexes = '';
+var allTextualHoldingsEnumeration = '';
 
 function startUpdatedMARCHoldings(rId) {
-
-
   getDetailPolicies();
-
   waitDetailMARCHoldings(rId);
 }
 
+function getDetailPolicies() {
+  var locationWsURL = baseWsURL + 'rest/admin/lookupLocationPolicyList?clientID=' + clientID + '&json=true&callback=?';
+  var libraryWsURL = baseWsURL + 'rest/admin/lookupLibraryPolicyList?clientID=' + clientID + '&json=true&callback=?';
+  var materialTypeWsURL = baseWsURL + 'rest/admin/lookupItemTypePolicyList?clientID=' + clientID + '&json=true&callback=?';
+  $J.getJSON(locationWsURL, function(locationData) {
+    $J.each(locationData.policyInfo, function(index, value) {
+      locationMap[this.policyID] = this.policyDescription;
+    });
+    locationDone = true;
+  });
+  $J.getJSON(libraryWsURL, function(libraryData) {
+    $J.each(libraryData.policyInfo, function(index, value) {
+      libraryMap[this.policyID] = this.policyDescription;
+    });
+    libraryDone = true;
+  });
+  $J.getJSON(materialTypeWsURL, function(materialTypeData) {
+    $J.each(materialTypeData.policyInfo, function(index, value) {
+      materialTypeMap[this.policyID] = this.policyDescription;
+    });
+    materialTypeDone = true;
+  });
+}
+
 function waitDetailMARCHoldings(rId) {
-
-
   if (!(libraryDone == true && materialTypeDone == true && locationDone == true)) {
     curAttempt++;
     if (curAttempt <= maxAttempts) {
@@ -35,42 +55,28 @@ function waitDetailMARCHoldings(rId) {
       }, 250);
     }
   } else {
-
     goDetailMARCHoldings(rId);
   }
 }
 
-
 function goDetailMARCHoldings(rId) {
-
   var numRows = 0;
-
   var hitNum = rId.split('detail')[1];
   var catKey = $J('#' + rId + '_DOC_ID .DOC_ID_value').text().split(':')[1];
   var attachTarget = jQuery('#detail_accordion' + hitNum + ' h3:contains("Holdings")').next();
   var wsURL = baseWsURL + 'rest/standard/lookupTitleInfo?clientID=' + clientID + '&titleID=' + catKey + '&includeMarcHoldings=true&marcEntryFilter=ALL&json=true&callback=?';
-
   $J.getJSON(wsURL, function(tr) {
-
     //Add MARC Holdings Info
-
     var holdingsInfo = tr.TitleInfo[0].MarcHoldingsInfo;
-
     if (holdingsInfo) {
-
       var htmlHoldingOutput = '<thead><tr><th class="detailItemsTable_LIBRARY"><div class="detailItemTable_th">Library</div></th><th class="detailItemsTable_LOCATION"><div class="detailItemTable_th">Shelf Location</div></th><th class="detailItemsTable_CALLNUMBER"><div class="detailItemTable_th">Call Number</div></th><th class="detailItemsTable_HOLDING"><div class="detailItemTable_th">LSU Has:</div></th></tr></thead><tbody>'
-
-
       $J.each(holdingsInfo, function(holdingIndex, holding) {
         var holdingLibID = this.holdingLibraryID;
         var holdingLibDesc = libraryMap[holdingLibID];
-
         var holdingEntry = this.MarcEntryInfo;
-
+        var allTextualHoldingsEnumeration = '';
         $J.each(holdingEntry, function(holdingEntryIndex, holdingEntryData) {
-
           var entryId = this.entryID;
-
           if (entryId === '852') {
             var holdingLocationText = this.text;
             holdingLocation = holdingLocationText.split('--')[0];
@@ -88,48 +94,38 @@ function goDetailMARCHoldings(rId) {
               holdingAddend = '';
             }
           }
-
           if (entryId === '866') {
-            textualHoldings += this.text + '<br />';
+            textualHoldings += "<p>" + this.text + "</p>";
           }
-
           if (entryId === '863') {
-            textualHoldingsEnumeration += this.text + '<br />';
+            textualHoldingsEnumeration += "<p>" + this.text + "</p>";
+            if (allTextualHoldingsEnumeration.length) {
+                allTextualHoldingsEnumeration += ', ' + this.text;
+            }
+            else {
+                allTextualHoldingsEnumeration = this.text;
+            }
           }
-
           if (entryId === '867') {
-            textualHoldingsSupplemental += this.label + ': ' + this.text + '<br />';
+            textualHoldingsSupplemental += "<p>" + this.label + ': ' + this.text + "</p>";
           }
-
           if (entryId === '868') {
-            textualHoldingsIndexes += this.label + ': ' + this.text + '<br />';
+            textualHoldingsIndexes += "<p>" + this.label + ': ' + this.text + "</p>";
           }
-
         });
-
-        htmlHoldingOutput += '<tr class="detailItemsTableRow"><td>' + holdingLibDesc + '</td><td>' + holdingLocationDesc + '</td><td><p>' + holdingShelfMark + '</p><p>' + holdingAddend + '</p></td><td>' + textualHoldings + textualHoldingsEnumeration + textualHoldingsSupplemental + textualHoldingsIndexes + '</td></tr>';
-
+        htmlHoldingOutput += '<tr class="detailItemsTableRow"><td class="detailItem library">' + holdingLibDesc + '</td><td class="detailItem location">' + holdingLocationDesc + '</td><td class="detailItem callno"><p>' + holdingShelfMark + '</p><p>' + holdingAddend.replace('Current Issues', allTextualHoldingsEnumeration) + '</p></td><td class="detailItem lsuHas">' + textualHoldings + textualHoldingsEnumeration + textualHoldingsSupplemental + textualHoldingsIndexes + '</td></tr>';
         //empty the variables
         textualHoldings = '';
         textualHoldingsEnumeration = '';
         textualHoldingsSupplemental = '';
         textualHoldingsIndexes = '';
-
       });
-
       htmlHoldingOutput += '</tbody><tfoot></tfoot>'
-
     } // end if holdingsInfo
     else {
       htmlHoldingOutput = '';
     }
-
-
-
     // End Add MARC Holdings Info
-
-
-
     if (htmlHoldingOutput !== '') {
       $J(attachTarget).find('#detailItemTable0').html(htmlHoldingOutput);
       /* 
@@ -138,40 +134,5 @@ function goDetailMARCHoldings(rId) {
       // go_go_gadget_sorttable();
       // sorttable.init(0);
     }
-
-
-
-  });
-
-
-
-}
-
-
-
-function getDetailPolicies() {
-  var locationWsURL = baseWsURL + 'rest/admin/lookupLocationPolicyList?clientID=' + clientID + '&json=true&callback=?';
-  var libraryWsURL = baseWsURL + 'rest/admin/lookupLibraryPolicyList?clientID=' + clientID + '&json=true&callback=?';
-  var materialTypeWsURL = baseWsURL + 'rest/admin/lookupItemTypePolicyList?clientID=' + clientID + '&json=true&callback=?';
-  $J.getJSON(locationWsURL, function(locationData) {
-    $J.each(locationData.policyInfo, function(index, value) {
-      locationMap[this.policyID] = this.policyDescription;
-
-    });
-    locationDone = true;
-  });
-
-  $J.getJSON(libraryWsURL, function(libraryData) {
-    $J.each(libraryData.policyInfo, function(index, value) {
-      libraryMap[this.policyID] = this.policyDescription;
-    });
-    libraryDone = true;
-
-  });
-  $J.getJSON(materialTypeWsURL, function(materialTypeData) {
-    $J.each(materialTypeData.policyInfo, function(index, value) {
-      materialTypeMap[this.policyID] = this.policyDescription;
-    });
-    materialTypeDone = true;
   });
 }
