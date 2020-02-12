ed26d5f358cfbc925a3cf6a28bc2130482ce6158
flummingbird <wwconlin@gmail.com>
2020-02-12 13:20:37 -0600

    post upgrade

--- FunctionalTests/_conf_settings.py
+++ FunctionalTests/_conf_settings.py
@@ -8,6 +8,7 @@ All tests will target the site specified here.
 URL = "https://lsu.ent.sirsi.net/client/en_US/lsu"  # custom.js  livesite develop branch of this module
 # URL = "https://lsu.ent.sirsi.net/client/en_US/dec2018_fork"  # custom-develop.js  livesite
 #
+#
 # USER_AGENT = "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1"
 # USER_AGENT = "Mozilla/5.0 (Linux; U; Android 4.4.2; en-us; SCH-I535 Build/KOT49H) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0"
